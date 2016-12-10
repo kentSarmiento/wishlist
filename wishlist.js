c8f67f27e0f092ba09d6acb74af4cda7aaac5d76
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2016-12-10 13:29:37 +0800

    Provided support for authentication

--- wishlist.js
+++ wishlist.js
@@ -2,6 +2,9 @@ const PORT_NUM = 8080;
 
 var http = require('http');
 var fs = require('fs');
+var querystring = require('querystring');
+var readline = require('readline');
+var eventemitter = require('events').EventEmitter;
 
 var server = http.createServer();
 
@@ -18,6 +21,7 @@ function request_handler(req, res) {
     case '/upload':
       console.log('Processing form');
       upload_form(req, res);
+      break;
     case '/view':
       console.log('Providing list');
       display_list(res);
@@ -37,7 +41,54 @@ function display_form(res) {
 }
 
 function upload_form(req, res) {
+  var body = '';
+  var authenticate = new eventemitter(); // For authentication process
 
+  req.on('data', function (chunk) {
+    body += chunk.toString();
+  });
+  req.on('end', function () {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    var dec = querystring.parse(body);
+    check_user(dec, authenticate);
+  });
+
+  authenticate.on('auth', function () {
+    res.write('<html><body>You are authorized!</body></html>');
+    res.end();
+  });
+  authenticate.on('unauth', function () {
+    res.write('<html><body>You are not authorized!</body></html>');
+    res.end();
+  });
+}
+
+function check_user(dec, authenticate) {
+  var ret = false;
+
+  /* Check if user is authorized */
+  var rl = readline.createInterface({input: fs.createReadStream('passwd')});
+  rl.on('line', function (line) {
+    line = querystring.parse(line);
+    if (dec.name == line.name && dec.password == line.password) {
+      ret = true;
+    }
+  });
+
+  rl.on('close', function () {
+    if (ret == true) {
+      authenticate.emit('auth');
+    }
+    else {
+      authenticate.emit('unauth');
+    }
+  });
+}
+
+function make_wish(dec) {
+  /* Write file for wish */
 }
 
 function display_list(res) {
